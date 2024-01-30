0821c9b23210ed68eeb6fef455a145667ebc29b7
Julian Lopez Padua <julianlopez43013@gmail.com>
2024-01-30 18:24:26 -0300

    detail html y logica terminado

--- src/components/RecipesDetail/CardRecipesDetail.tsx
+++ src/components/RecipesDetail/CardRecipesDetail.tsx
@@ -1,3 +1,50 @@
-export default function CardRecipesDetail() {
-  return <div>CardRecipesDetail</div>;
+interface Props {
+  nombre: string;
+  tiempo: string;
+  ingredientes: string[];
+  pasos: string;
+  dificultad: number;
+  imagen: string;
+}
+
+export default function CardRecipesDetail({
+  dificultad,
+  imagen,
+  ingredientes,
+  nombre,
+  pasos,
+  tiempo,
+}: Props) {
+  return (
+    <section>
+      <h2 className="text-center text-2xl">{nombre}</h2>
+      <img src={imagen} alt="imagen" className="max-w-[400px]" />
+
+      <article className="flex items-center justify-around px-8">
+        <article className="flex flex-col justify-center">
+          <span>Tiempo de preparacion</span>
+          <h5>{tiempo}</h5>
+        </article>
+
+        <article className="flex flex-col  justify-center">
+          <h5>Nivel de dificultad</h5>
+          <span>{dificultad}</span>
+        </article>
+      </article>
+
+      <article>
+        <h3>Ingredientes</h3>
+        <ul className="flex flex-col my-12 list-disc ml-6">
+          {ingredientes.map((i, key) => {
+            return <li key={key}>{i}</li>;
+          })}
+        </ul>
+      </article>
+
+      <article>
+        <h3 className="font-bold">Elaboracion</h3>
+        <p>{pasos}</p>
+      </article>
+    </section>
+  );
 }
--- src/vistas/RecipesDetail.tsx
+++ src/vistas/RecipesDetail.tsx
@@ -2,15 +2,27 @@ import { useParams } from "react-router-dom";
 
 import MainRecipesDetail from "../components/RecipesDetail/MainRecipesDetail";
 import { recetas } from "../data/recetas";
+import CardRecipesDetail from "../components/RecipesDetail/CardRecipesDetail";
 
 export default function RecipesDetail() {
   const { id } = useParams();
 
   const card = recetas.find((c) => c.id === Number(id));
-  console.log(card);
+
+  if (!card) {
+    return <h2>No hay card</h2>;
+  }
   return (
     <>
       <MainRecipesDetail />
+      <CardRecipesDetail
+        dificultad={card.dificultad}
+        imagen={card.imagen}
+        ingredientes={card?.ingredientes}
+        nombre={card.nombre}
+        pasos={card.pasos}
+        tiempo={card.tiempo}
+      />
     </>
   );
 }
