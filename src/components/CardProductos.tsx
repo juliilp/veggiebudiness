eda6a39f987e86c8f3be6166d28ee4c757db86c5
Julian Lopez Padua <julianlopez43013@gmail.com>
2024-02-03 20:27:52 -0300

    prettier

--- src/components/CardProductos.tsx
+++ src/components/CardProductos.tsx
@@ -8,7 +8,12 @@ export default function CardProductos({
   return (
     <article className=" flex flex-col items-center justify-center gap-6 lg:h-[420px] ">
       <h2 className="font-aleo text-xl text-center">{nombre}</h2>
-      <img src={imagen} alt="Imagen" className="w-[300px] h-[250px] px-4" />
+      <img
+        src={imagen}
+        alt="Imagen"
+        className="w-[300px] h-[250px] px-4"
+        loading="lazy"
+      />
       <p className="font-aleo text-center max-w-[400px] ">{subnombre}</p>
     </article>
   );
