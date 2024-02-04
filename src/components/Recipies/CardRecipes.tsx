83ad08b3c4deceb78d088ff6d7b0079c77a699fb
Julian Lopez Padua <julianlopez43013@gmail.com>
2024-02-03 22:22:22 -0300

    card detail

--- src/components/Recipies/CardRecipes.tsx
+++ src/components/Recipies/CardRecipes.tsx
@@ -5,17 +5,11 @@ interface Props {
   texto: string;
   url: number;
 }
+
 export default function CardRecipes({ imagen, texto, url }: Props) {
   return (
     <Link to={`/recipies/${url}`}>
-      <article className="mx-8 md:px-0 flex flex-col items-center md:flex-row md:gap-8 lg:gap-24  ">
-        <p
-          className={`${
-            url % 2 != 0 && "md:order-2 md:text-right "
-          } max-w-[400px] text-center `}
-        >
-          {texto}
-        </p>
+      <article className="flex flex-col items-center md:flex-row gap-4 md:gap-8 lg:gap-24 shadow-2xl py-8 px-8 rounded-md  ">
         <Link
           to={`/recipies/${url}`}
           className="cursor-pointer transition-all duration-300 hover:scale-105 flex items-center justify-center"
@@ -23,10 +17,17 @@ export default function CardRecipes({ imagen, texto, url }: Props) {
           <img
             src={imagen}
             alt="imagen"
-            className=" w-full max-w-[500px] h-[400px] rounded-2xl"
+            className="w-[350px] h-[350px] md:w-[400px] object-cover rounded-2xl "
             loading="lazy"
           />
         </Link>
+        <p
+          className={`${
+            url % 2 != 0 && "md:-order-2 md:text-right "
+          } max-w-[400px] text-left text-sm  p-4 md:p-0 `}
+        >
+          {texto}
+        </p>
       </article>
     </Link>
   );
