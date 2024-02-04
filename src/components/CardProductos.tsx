9791fed5f8d84c1f6c87222642489cd556685393
Julian Lopez Padua <julianlopez43013@gmail.com>
2024-02-03 21:34:55 -0300

    tamaño imagenes productos

--- src/components/CardProductos.tsx
+++ src/components/CardProductos.tsx
@@ -6,15 +6,19 @@ export default function CardProductos({
   subnombre,
 }: Productos) {
   return (
-    <article className=" flex flex-col items-center justify-center gap-6 lg:h-[420px] ">
-      <h2 className="font-aleo text-xl text-center">{nombre}</h2>
+    <article className=" flex flex-col items-center justify-center gap-6  ">
+      <h2 className="font-aleo text-xl text-center md:text-2xl font-normal">
+        {nombre}
+      </h2>
       <img
         src={imagen}
         alt="Imagen"
-        className="w-[300px] h-[250px] px-4"
+        className="w-full max-w-[450px] h-[350px] px-4"
         loading="lazy"
       />
-      <p className="font-aleo text-center max-w-[400px] ">{subnombre}</p>
+      <p className="font-aleo text-center max-w-[400px] text-lg ">
+        {subnombre}
+      </p>
     </article>
   );
 }
