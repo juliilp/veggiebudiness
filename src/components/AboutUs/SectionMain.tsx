48ddff35538475e86d8f8532d690bc9dc6554919
Julian Lopez Padua <julianlopez43013@gmail.com>
2024-02-16 19:12:02 -0300

    add main

---
+++ src/components/AboutUs/SectionMain.tsx
@@ -0,0 +1,25 @@
+import mainFondo from "../../assets/Recipies/fondo-recipies.jpg";
+
+export default function SectionMain() {
+  return (
+    <main className="w-full h-screen flex justify-center items-center relative overflow-hidden">
+      <div
+        style={{
+          backgroundImage: `url(${mainFondo})`,
+        }}
+        className="absolute w-full h-screen bg-cover bg-center bg-fixed"
+      />
+      <section className="text-center text-white relative z-10">
+        <span className="block text-[#FFE500] font-bevan text-3xl lg:text-4xl">
+          Disfruta (re)creando
+        </span>
+        <span className="block font-bevan text-5xl lg:text-6xl">
+          Nuestras recetas
+        </span>
+        <span className="block font-playfairDisplay text-3xl lg:text-4xl">
+          Deja volar tu imaginación y dale vida a deliciosas preparaciones
+        </span>
+      </section>
+    </main>
+  );
+}
--- src/vistas/AboutUs.tsx
+++ src/vistas/AboutUs.tsx
@@ -1,10 +1,12 @@
 import Section1 from "../components/AboutUs/Section1";
 import Section2 from "../components/AboutUs/Section2";
 import Section3 from "../components/AboutUs/Section3";
+import SectionMain from "../components/AboutUs/SectionMain";
 
 export default function AboutUs() {
   return (
-    <main className="flex flex-col gap-16 md:gap-32 py-16 md:py-32 ">
+    <main className="flex flex-col gap-16 md:gap-32  ">
+      <SectionMain />
       <Section1 />
       <Section2 />
       <Section3 />
