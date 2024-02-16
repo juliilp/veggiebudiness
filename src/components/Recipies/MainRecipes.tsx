c7295550e599077d22cd702e2e1cac84ede81504
Julian Lopez Padua <julianlopez43013@gmail.com>
2024-02-16 17:57:07 -0300

    agregue la imagen

--- src/components/Recipies/MainRecipes.tsx
+++ src/components/Recipies/MainRecipes.tsx
@@ -1,14 +1,21 @@
+import mainFondo from "../../assets/Recipies/fondo-recipies.jpg";
+
 export default function MainRecipes() {
   return (
-    <main className="w-full h-screen md:flex md:justify-center md:items-center px-4 md:px-0">
-      <section className="my-8 md:my-0 text-center">
-        <span className="block text-[#FFE500] font-bevan text-xl md:text-3xl">
+    <main className="w-full h-screen flex justify-center items-center relative">
+      <img
+        src={mainFondo}
+        alt="imagen-fondo"
+        className="absolute w-full h-screen object-cover -z-10 brightness-[0.5]"
+      />
+      <section className="text-center text-white">
+        <span className="block text-[#FFE500] font-bevan text-3xl lg:text-4xl">
           Disfruta (re)creando
         </span>
-        <span className="block font-bevan text-3xl md:text-5xl ">
+        <span className="block font-bevan text-5xl lg:text-6xl ">
           Nuestras recetas
         </span>
-        <span className="block font-playfairDisplay text-xl md:text-3xl">
+        <span className="block font-playfairDisplay text-3xl lg:text-4xll">
           Deja volar tu imaginación y dale vida a deliciosas preparaciones
         </span>
       </section>
